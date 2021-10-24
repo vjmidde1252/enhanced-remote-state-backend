6f57f93708d50d6da77aaa382a68d436092cc116
Vijay Midde <vijay.midde89@gmail.com>
2021-10-24 00:46:00 -0500

    first commit again

---
+++ main.tf
@@ -0,0 +1,12 @@
+#Importing module from terrform registry
+module "module-apachedemo" {
+  source  = "vjmidde1252/module-apachedemo/aws"
+  version = "1.0.0"
+  # insert the 3 required variables here
+  vpc_id        = var.vpc_id
+  my_ip         = var.my_ip
+  EC2-Key       = var.EC2-Key
+  instance_type = var.instance_type
+  server_name   = var.server_name
+
+}
---
+++ providers.tf
@@ -0,0 +1,22 @@
+#Welcome Vijay Midde
+terraform {  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "remote" {
+    organization = "midde-org"
+
+    workspaces {
+      name = "enhanced-remote-state-backend"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+  profile = "default"
+}
